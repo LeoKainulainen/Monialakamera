c6aa48a319dfb8ce463a4e0874a097f6fdc7d684
LeoKainulainen <36257917+LeoKainulainen@users.noreply.github.com>
2019-02-19 05:03:24 +0200

    Update tkintertest.py

--- tkintertest.py
+++ tkintertest.py
@@ -12,8 +12,8 @@ img_path1 = Path("test_images") / "1-peloton-finishlynx.png"
 im = Image.open(img_path1)
 im = im.resize((1000, 483), Image.ANTIALIAS) 
 pic = ImageTk.PhotoImage(im)
-
 stamp_list = []
+
 def create_stamps(stamp_list):
     x, y = im.size
     date = datetime.now()
@@ -22,37 +22,23 @@ def create_stamps(stamp_list):
         date_combo = date + timedelta(seconds=i)
         time_stamp = date_combo.strftime('%M:%S')
         stamp_list.append(time_stamp)
-    
-        
+   
 canvas = Canvas(root, width = pic.width(), height = pic.height() )
 canvas.create_image(0, 0, anchor = NW, image=pic)
-
-
-
-
 
 def leftClick(event):
     print("left")
     canvas.delete('text')
     canvas.create_line(event.x, 0, event.x, pic.height(), tag='line')
     
-    
-    
 def deleteText():
     print("Deleted lines")
     canvas.delete('line')
-
-
 
 def motion(event):
     x = event.x
     T.delete('1.0', END)
     T.insert('1.0', "Time of the frame is " + str(stamp_list[event.x]))
-
-
-    
-    
-    
 
 B = Button(text ="Delete lines", command = deleteText)
 canvas.bind("<Button-1>", leftClick)
