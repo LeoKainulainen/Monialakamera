3af46fb605e48b0c731ddc103b112625b0e063c5
rkantos <robin.karinen@gmail.com>
2019-03-04 05:18:19 +0200

    todo //trying automatic recording with contrast checking

---
+++ LinescanDetect/contrast_delta_record.py
@@ -0,0 +1,9 @@
+"""
+functions for recording IDS cameras as linescan mode with automatic movement / contrast delta recognition
+"""
+
+# UI configurable value
+gui_threshold = 50 # Value between 0 and 100
+
+delta_value = / 100 # Value to decimal
+
